59b8d3f4d28f18eb6770d2133e8cd609a668d28f
Gabriel Lopes <lopes@hey.com>
2024-08-20 15:25:49 -0700

    add tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,41 @@ import {
   DownloadIcon,
   LoopIcon,
   Share2Icon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
 } from "@radix-ui/react-icons";
 
+type AspectRatioOption = "9:16" | "1:1" | "16:9";
+
 function App() {
+  const [currentTab, setCurrentTab] = useState("face");
+
+  const tabs: { name: string; value: string }[] = [
+    { name: "Face", value: "face" },
+    { name: "Hair", value: "hair" },
+    { name: "Eyes", value: "eyes" },
+    { name: "Nose", value: "nose" },
+    { name: "Mouth", value: "mouth" },
+    { name: "Facial", value: "facial" },
+    { name: "Accessories", value: "accessories" },
+  ];
+
+  const currentIndex = tabs.findIndex((tab) => tab.value === currentTab);
+  const hasPrevious = currentIndex > 0;
+  const hasNext = currentIndex + 1 < tabs.length;
+
+  const onPrevious = () => {
+    if (hasPrevious) {
+      const previousIndex = currentIndex - 1;
+      setCurrentTab(tabs[previousIndex].value);
+    }
+  };
+
+  const onNext = () => {
+    const nextIndex = currentIndex < tabs.length - 1 ? currentIndex + 1 : 0;
+    setCurrentTab(tabs[nextIndex].value);
+  };
+
   const [face, setFace] = useState(0);
   const faces = [0, 1, 2, 3];
 
@@ -41,11 +73,12 @@ function App() {
     "#0E0705",
   ];
 
-  const onDownload = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onDownload = (_aspectRatio: AspectRatioOption) => {
     domtoimage
       .toBlob(document.getElementById("avatar-preview")!)
       .then(function (blob) {
-        saveAs(blob, "my-node.png");
+        saveAs(blob, "sentry-avatar.png");
       });
   };
 
@@ -60,23 +93,24 @@ function App() {
           </div>
 
           <div className="order-1 sm:col-span-3 bg-white rounded-lg">
-            <Tabs defaultValue="face">
-              <ScrollArea className="bg-card rounded-t-md">
+            <Tabs
+              defaultValue="face"
+              className="flex flex-col h-full"
+              value={currentTab}
+              onValueChange={(value) => setCurrentTab(value)}
+            >
+              <ScrollArea className="bg-card rounded-t-md w-full">
                 <div className="w-0">
                   <TabsList className="bg-transparent p-0 items-stretch">
-                    <TabsTrigger value="face">Face</TabsTrigger>
-                    <TabsTrigger value="hair">Hair</TabsTrigger>
-                    <TabsTrigger value="eyes">Eyes</TabsTrigger>
-                    <TabsTrigger value="nose">Nose</TabsTrigger>
-                    <TabsTrigger value="mouth">Mouth</TabsTrigger>
-                    <TabsTrigger value="facial">Facial</TabsTrigger>
-                    <TabsTrigger value="accessories">Accessories</TabsTrigger>
+                    {tabs.map((tab) => (
+                      <TabsTrigger value={tab.value}>{tab.name}</TabsTrigger>
+                    ))}
                   </TabsList>
                 </div>
                 <ScrollBar orientation="horizontal" />
               </ScrollArea>
 
-              <TabsContent value="face">
+              <TabsContent value="face" className="grow">
                 <AvatarGridOptions
                   values={faces}
                   currentValue={face}
@@ -85,7 +119,7 @@ function App() {
                 />
               </TabsContent>
 
-              <TabsContent value="hair" className="space-y-3">
+              <TabsContent value="hair" className="grow">
                 <AvatarGridOptions
                   values={hairColors}
                   currentValue={hairColor}
@@ -98,6 +132,28 @@ function App() {
                   setColor={setHairColorNew}
                 />
               </TabsContent>
+
+              <div>
+                {hasPrevious && (
+                  <Button
+                    variant="link"
+                    onClick={() => onPrevious()}
+                    className="float-left font-bold text-base px-2"
+                  >
+                    <ChevronLeftIcon className="h-5 w-5" /> Prev{" "}
+                  </Button>
+                )}
+
+                {hasNext && (
+                  <Button
+                    variant="link"
+                    onClick={() => onNext()}
+                    className="float-right font-bold text-base px-2"
+                  >
+                    Next <ChevronRightIcon className="h-5 w-5" />
+                  </Button>
+                )}
+              </div>
             </Tabs>
           </div>
 
@@ -114,17 +170,29 @@ function App() {
               </DropdownMenuTrigger>
               <DropdownMenuContent>
                 <DropdownMenuItem asChild>
-                  <Button variant="ghost" className="w-full justify-between">
+                  <Button
+                    variant="ghost"
+                    className="w-full justify-between"
+                    onClick={() => onDownload("9:16")}
+                  >
                     9:16 <DownloadIcon />
                   </Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                  <Button variant="ghost" className="w-full justify-between">
+                  <Button
+                    variant="ghost"
+                    className="w-full justify-between"
+                    onClick={() => onDownload("1:1")}
+                  >
                     1:1 <DownloadIcon />
                   </Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                  <Button variant="ghost" className="w-full justify-between">
+                  <Button
+                    variant="ghost"
+                    className="w-full justify-between"
+                    onClick={() => onDownload("16:9")}
+                  >
                     16:9 <DownloadIcon />
                   </Button>
                 </DropdownMenuItem>
